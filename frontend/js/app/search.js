4cf3b7d9286978b2624457c0e9424ad59c7d82cd
akudelka <alexey.kudelka@gmail.com>
2017-09-10 18:14:32 +0300

    boer json

--- frontend/js/app/search.js
+++ frontend/js/app/search.js
@@ -3,20 +3,20 @@ appModule.controller('SearchController', ['$resource', '$scope', '$rootScope', f
     // console.log($scope.t);
     $scope.search = function(search){
         // console.log($scope.types[$scope.t].replace('^', search));
-        // search = search || '';
-        if ($scope.t === 'def'){
+        search = search || '*';
+        if (search && $scope.t === 'def'){
             $scope.t = 'post'
         }
         search = $scope.types[$scope.t].replace('^', search);
-        $scope.results = $resource("http://askgolos.ru/search?indent=on&wt=json&q=" + encodeURIComponent(search)).get();
+        $scope.results = $resource("http://askgolos.ru/search?indent=on&wt=json&q=" + encodeURIComponent(search), function(){}).get();
         // console.log($scope.t);
         // $http.get("http://195.2.252.217:9999/solr/askgolos/select?wt=json&q=" + encodeURIComponent(search), function(e, data){
         //     $scope.results = data;
         // })
     };
     $scope.set_type = function(t){
-
         $scope.t = t;
+        $scope.search($scope.search_text);
         // console.log($scope.t);
 
         // $scope.search_text
@@ -26,7 +26,8 @@ appModule.controller('SearchController', ['$resource', '$scope', '$rootScope', f
         def: "depth:0",
         post: "^ AND depth:0",
         account: "^ AND ns:steemdb.account",
-        comments: "^ AND depth>0"
+        comments: "^ AND depth>0",
+        archive: "^ AND depth:0 AND mode:archived"
         // byvote: ""
     }
 }]);
